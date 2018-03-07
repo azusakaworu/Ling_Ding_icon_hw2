96a5909de6af77a3bdddee649db1349318eadd6f
Ding <l_ding5@tss-hr420-dm08.fc.ca>
2018-03-07 17:00:04 -0500

    update

--- js/main.js
+++ js/main.js
@@ -5,22 +5,21 @@
 (() => {
  console.log("svg script file laded");
  let
-    svgs = document.querySelectorALL("svg");
+    svgs = document.querySelector("svg");
 
  function logThisSvg(){
         console.log(this.id);
     }
 
  function bigger(){
-       this.style.width="100%";
+       this.style.width= '100%';
     }
 
  function small(){
-      this.style.width= "40%";
+      this.style.width= '80%';
     }
- svgs.forEach(svg =>
-	 svg.addEventListener("click", logThisSvg);
-	 svg.addEventListener("mouseover", bigger);
-   svg.addEventListener("mouseout", small));
+svgs.addEventListener('click', logThisSvg);
+svgs.addEventListener('mouseover', bigger);
+svgs.addEventListener('mouseout', small);
 
 })();
